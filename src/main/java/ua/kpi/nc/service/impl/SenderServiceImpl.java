9e141b594534d3429a210502fccd988aed321f55
MynaDima <dmytromyna@gmail.com>
2016-04-13 18:40:37 +0300

    add mail sender service and social-google auth service and controller

---
+++ src/main/java/ua/kpi/nc/service/impl/SenderServiceImpl.java
@@ -0,0 +1,30 @@
+package ua.kpi.nc.service.impl;
+
+import org.springframework.stereotype.Service;
+import ua.kpi.nc.model.Sender;
+import ua.kpi.nc.model.User;
+import ua.kpi.nc.service.SenderService;
+
+import java.util.List;
+
+/**
+ * Created by dima on 13.04.16.
+ */
+@Service
+public class SenderServiceImpl implements SenderService {
+
+    private static Sender tlsSender = new Sender("nckpiua@gmail.com", "*******");
+
+    @Override
+    public void send(User user, String subject, String text ) {
+        tlsSender.send(subject, text, user.getUsername());
+    }
+
+    @Override
+    public void send(List<User> userList, String subject, String text ) {
+        for (User user : userList) {
+            tlsSender.send(subject, text, user.getUsername());
+        }
+    }
+
+}
