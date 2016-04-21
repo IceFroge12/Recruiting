66e8b843d9c2a8ee1f27d10be62b9d3ad1cae5c4
Sajel <Sajel@Sajel-PC>
2016-04-21 17:02:06 +0300

    Add Decision, EmailTemplaye, NotificationType model classes

---
+++ src/main/java/ua/kpi/nc/persistence/model/EmailTemplate.java
@@ -0,0 +1,71 @@
+package ua.kpi.nc.persistence.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class EmailTemplate {
+
+	private Long id;
+
+	private String title;
+
+	private NotificationType notificationType;
+
+	public EmailTemplate() {
+	}
+
+	public EmailTemplate(Long id, String title, NotificationType notificationType) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.notificationType = notificationType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public NotificationType getNotificationType() {
+		return notificationType;
+	}
+
+	public void setNotificationType(NotificationType notificationType) {
+		this.notificationType = notificationType;
+	}
+
+	@Override
+	public String toString() {
+		return "EmailTemplate [id=" + id + ", title=" + title + ", notificationType=" + notificationType + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).append(id).append(notificationType).append(title).toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmailTemplate other = (EmailTemplate) obj;
+		return new EqualsBuilder().append(id, other.id).append(title, other.title)
+				.append(notificationType, other.notificationType).isEquals();
+	}
+
+}
---
+++ src/main/java/ua/kpi/nc/persistence/model/NotificationType.java
@@ -0,0 +1,59 @@
+package ua.kpi.nc.persistence.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class NotificationType {
+
+	private Long id;
+
+	private String title;
+
+	public NotificationType() {
+	}
+
+	public NotificationType(Long id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "NotificationType [id=" + id + ", title=" + title + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).append(id).append(title).toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotificationType other = (NotificationType) obj;
+		return new EqualsBuilder().append(id, other.id).append(title, other.title).isEquals();
+	}
+
+}
