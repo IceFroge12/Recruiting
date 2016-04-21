dc563ff1cfa64cb25b682b7f6cd8a93128efae19
Алексей <kozakaleksei@gmail.com>
2016-04-21 18:07:35 +0300

    add ApplicationForm interface

---
+++ src/main/java/ua/kpi/nc/persistence/model/impl/real/FormAnswerImpl.java
@@ -0,0 +1,12 @@
+package ua.kpi.nc.persistence.model.impl.real;
+
+import ua.kpi.nc.persistence.model.FormAnswer;
+
+import java.io.Serializable;
+
+/**
+ * Created by Алексей on 21.04.2016.
+ */
+public class FormAnswerImpl implements FormAnswer{
+
+}
