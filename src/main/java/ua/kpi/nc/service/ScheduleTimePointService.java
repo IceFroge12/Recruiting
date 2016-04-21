b34e551947acb79390553af1ad50d49c6e6ef8bc
Sajel <Sajel@Sajel-PC>
2016-04-21 21:16:06 +0300

    ScheduleTimePointService upd

--- src/main/java/ua/kpi/nc/service/ScheduleTimePointService.java
+++ src/main/java/ua/kpi/nc/service/ScheduleTimePointService.java
@@ -1,7 +1,8 @@
 package ua.kpi.nc.service;
-
+import ua.kpi.nc.persistence.model.ScheduleTimePoint;
 /**
  * Created by Chalienko on 21.04.2016.
  */
 public interface ScheduleTimePointService {
+	ScheduleTimePoint getScheduleTimePointById(Long id);
 }
