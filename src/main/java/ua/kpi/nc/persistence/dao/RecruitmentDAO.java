dee6ad0267850b1e20c773328e67653040e47afc
Korzh Vladimir <korzh.volod@gmail.com>
2016-04-21 21:36:55 +0300

    DAO Interfaces update

---
+++ src/main/java/ua/kpi/nc/persistence/dao/RecruitmentDAO.java
@@ -0,0 +1,22 @@
+package ua.kpi.nc.persistence.dao;
+
+import ua.kpi.nc.persistence.model.Recruitment;
+
+import java.sql.Connection;
+
+/**
+ * Created by Vova on 21.04.2016.
+ */
+public interface RecruitmentDAO  {
+
+    Recruitment getRecruitmentById(Long id);
+
+    Recruitment getRecruitmentByName(String name);
+
+    boolean updateRecruitment(Recruitment recruitment);
+
+    boolean addRecruitment(Recruitment recruitment);
+
+    int deleteRecruitment(Recruitment recruitment);
+
+}
