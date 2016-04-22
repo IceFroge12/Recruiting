59b5f2e1fac4d33932a72e721cd66b5bcf076d18
Sajel <Sajel@Sajel-PC>
2016-04-22 19:58:52 +0300

    ApplicationFormDao

---
+++ src/main/java/ua/kpi/nc/persistence/dao/ApplicationFormDao.java
@@ -0,0 +1,23 @@
+package ua.kpi.nc.persistence.dao;
+
+import java.util.Set;
+
+import ua.kpi.nc.persistence.model.ApplicationForm;
+import ua.kpi.nc.persistence.model.User;
+
+public interface ApplicationFormDao {
+
+	ApplicationForm getById(Long id);
+
+	Set<ApplicationForm> getByUserId(Long id);
+
+	Set<ApplicationForm> getByStatus(String status);
+
+	Set<ApplicationForm> getByState(boolean state);
+
+	int deleteApplicationForm(ApplicationForm applicationForm);
+
+	Long insertApplicationForm(ApplicationForm applicationForm, User user);
+
+	int updatApplicationForm(ApplicationForm applicationForm);
+}
