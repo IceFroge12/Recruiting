9e6ef792518d9533a55e5ac07aab1563b751d4a9
uakruk <uakruk@ukr.net>
2016-04-23 13:49:44 +0300

    Implemented FormAnswer services

--- src/main/java/ua/kpi/nc/service/impl/FormAnswerServiceImpl.java
+++ src/main/java/ua/kpi/nc/service/impl/FormAnswerServiceImpl.java
@@ -1,5 +1,9 @@
 package ua.kpi.nc.service.impl;
 
+import ua.kpi.nc.persistence.dao.FormAnswerDao;
+import ua.kpi.nc.persistence.model.FormAnswer;
+import ua.kpi.nc.service.FormAnswerService;
+
 /**
  * @author Yaroslav Kruk on 4/23/16.
  *         e-mail: yakruck@gmail.com
@@ -7,5 +11,16 @@ package ua.kpi.nc.service.impl;
  * @version 1.0
  * @since 1.7
  */
-public class FormAnswerServiceImpl {
+public class FormAnswerServiceImpl implements FormAnswerService {
+
+    private FormAnswerDao formAnswerDao;
+
+    public FormAnswerServiceImpl(FormAnswerDao formAnswerDao) {
+        this.formAnswerDao = formAnswerDao;
+    }
+
+    @Override
+    public FormAnswer getFormAnswerByID(Long id) {
+        return formAnswerDao.getById(id);
+    }
 }
--- src/main/java/ua/kpi/nc/service/impl/FormAnswerVariantServiceImpl.java
+++ src/main/java/ua/kpi/nc/service/impl/FormAnswerVariantServiceImpl.java
@@ -1,5 +1,9 @@
 package ua.kpi.nc.service.impl;
 
+import ua.kpi.nc.persistence.dao.FormAnswerVariantDao;
+import ua.kpi.nc.persistence.model.FormAnswerVariant;
+import ua.kpi.nc.service.FormAnswerVariantService;
+
 /**
  * @author Yaroslav Kruk on 4/23/16.
  *         e-mail: yakruck@gmail.com
@@ -7,5 +11,16 @@ package ua.kpi.nc.service.impl;
  * @version 1.0
  * @since 1.7
  */
-public class FormAnswerVariantServiceImpl {
+public class FormAnswerVariantServiceImpl implements FormAnswerVariantService {
+
+    private FormAnswerVariantDao formAnswerVariantDao;
+
+    public FormAnswerVariantServiceImpl(FormAnswerVariantDao formAnswerVariantDao) {
+        this.formAnswerVariantDao = formAnswerVariantDao;
+    }
+
+    @Override
+    public FormAnswerVariant getFormAnswerVariantById(Long id) {
+        return formAnswerVariantDao.getById(id);
+    }
 }
