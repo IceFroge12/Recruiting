210d6cd415dd586c5c5f1d4e3fb08baf26a07d3f
Алексей <kozakaleksei@gmail.com>
2016-04-23 16:18:31 +0300

    refact DecisionDaoImpl

--- src/main/java/ua/kpi/nc/service/StatusService.java
+++ src/main/java/ua/kpi/nc/service/StatusService.java
@@ -1,7 +1,16 @@
 package ua.kpi.nc.service;
 
+import ua.kpi.nc.persistence.model.Status;
+
 /**
  * Created by Chalienko on 21.04.2016.
  */
 public interface StatusService {
+    Status getStatusById(Long id);
+
+    int insertStatus(Status status);
+
+    int updateStatus(Status status);
+
+    int deleteStatus(Status status);
 }
