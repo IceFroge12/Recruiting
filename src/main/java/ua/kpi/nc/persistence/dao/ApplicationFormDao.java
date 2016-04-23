4bd53a9ae2e77ed223dbc2003a6690a8dfa25da7
mendejleeb <mendejleeb@gmail.com>
2016-04-24 02:19:56 +0300

    Add getAll for ApplicationFormDAO

--- src/main/java/ua/kpi/nc/persistence/dao/ApplicationFormDao.java
+++ src/main/java/ua/kpi/nc/persistence/dao/ApplicationFormDao.java
@@ -1,24 +1,26 @@
 package ua.kpi.nc.persistence.dao;
 
-import java.sql.Connection;
-import java.util.Set;
-
 import ua.kpi.nc.persistence.model.ApplicationForm;
 import ua.kpi.nc.persistence.model.User;
 
+import java.sql.Connection;
+import java.util.Set;
+
 public interface ApplicationFormDao {
 
-	ApplicationForm getById(Long id);
+    ApplicationForm getById(Long id);
 
-	Set<ApplicationForm> getByUserId(Long id);
+    Set<ApplicationForm> getByUserId(Long id);
 
-	Set<ApplicationForm> getByStatus(String status);
+    Set<ApplicationForm> getByStatus(String status);
 
-	Set<ApplicationForm> getByState(boolean state);
+    Set<ApplicationForm> getByState(boolean state);
 
-	int deleteApplicationForm(ApplicationForm applicationForm);
+    int deleteApplicationForm(ApplicationForm applicationForm);
 
-	Long insertApplicationForm(ApplicationForm applicationForm, User user, Connection connection);
+    Long insertApplicationForm(ApplicationForm applicationForm, User user, Connection connection);
 
-	int updateApplicationForm(ApplicationForm applicationForm);
+    int updateApplicationForm(ApplicationForm applicationForm);
+
+    Set<ApplicationForm> getAll();
 }
