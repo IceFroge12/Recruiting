4714f33fc30c8983bdc2812295abf540ab14385f
uakruk <uakruk@ukr.net>
2016-04-23 16:28:23 +0300

    Implemeted FormAnswerVariant, TimePriorityType services

---
+++ src/main/java/ua/kpi/nc/service/impl/FormQuestionServiceImpl.java
@@ -0,0 +1,11 @@
+package ua.kpi.nc.service.impl;
+
+/**
+ * @author Yaroslav Kruk on 4/23/16.
+ *         e-mail: yakruck@gmail.com
+ *         GitHub: https://github.com/uakruk
+ * @version 1.0
+ * @since 1.7
+ */
+public class FormQuestionServiceImpl {
+}
