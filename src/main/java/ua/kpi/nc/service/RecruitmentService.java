a633f6e596a3529de71b19b8dc03931438ade352
Korzh Vladimir <korzh.volod@gmail.com>
2016-04-24 13:47:17 +0300

    QuestionType refactor Recruitment Service done

--- src/main/java/ua/kpi/nc/service/RecruitmentService.java
+++ src/main/java/ua/kpi/nc/service/RecruitmentService.java
@@ -5,10 +5,18 @@ import ua.kpi.nc.persistence.model.Recruitment;
 import java.util.List;
 
 /**
- * Created by Chalienko on 21.04.2016.
+ * @author Chalienko  22.04.2016.
  */
 public interface RecruitmentService {
     Recruitment getRecruitmentById(Long id);
+
+    Recruitment getRecruitmentByName(String name);
+
+    int updateRecruitment(Recruitment recruitment);
+
+    boolean addRecruitment(Recruitment recruitment);
+
+    int deleteRecruitment(Recruitment recruitment);
 
     List<Recruitment> getAll();
 }
