0a85612de9275e1a87bb2db0c66a297d6464c7ae
Sajel <sajel15@gmail.com>
2016-04-28 23:24:24 +0300

    Renderer update

--- src/main/java/ua/kpi/nc/report/renderer/RendererFactory.java
+++ src/main/java/ua/kpi/nc/report/renderer/RendererFactory.java
@@ -1,13 +1,16 @@
 package ua.kpi.nc.report.renderer;
 
+import ua.kpi.nc.report.poi.RenderXLS;
+import ua.kpi.nc.report.poi.RenderXLSX;
+
 public class RendererFactory {
 
 	public static ReportRenderer getXLSRenderer() {
-		return null;
+		return new RenderXLS("121.xls");
 	}
 
 	public static ReportRenderer getXLSXRenderer() {
-		return null;
+		return new RenderXLSX("121.xlsx");
 	}
 
 	public static ReportRenderer getJSONRenderer() {
