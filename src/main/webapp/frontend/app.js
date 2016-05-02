f98666326602f51e87d8cfa0cd77bbaa1b4a95be
Sajel <sajel15@gmail.com>
2016-05-02 20:02:00 +0300

    feedback template fix2

--- src/main/webapp/frontend/app.js
+++ src/main/webapp/frontend/app.js
@@ -8,7 +8,8 @@ angular.module('app', [
     'appHome',
     'appAdmin',
     'appMain',
-    'appForm'
+    'appForm',
+    'appFeedback'
 ]).config(function ($routeProvider) {
 
     $routeProvider
