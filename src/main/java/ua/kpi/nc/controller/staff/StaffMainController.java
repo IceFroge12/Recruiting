fff78da5dae95c238bbf1873d3600dc7c0a2358d
uakruk <uakruk@ukr.net>
2016-05-02 20:38:05 +0300

    Started great jsp and controller refactoring for staff

---
+++ src/main/java/ua/kpi/nc/controller/staff/StaffMainController.java
@@ -0,0 +1,14 @@
+package ua.kpi.nc.controller.staff;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by uakruk on 5/2/16.
+ */
+@Controller
+@RequestMapping("/staff**")
+public class StaffMainController {
+
+
+}
