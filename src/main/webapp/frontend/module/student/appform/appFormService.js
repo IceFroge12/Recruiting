67a9fa4a2aa56af746b359cfe1701958e87b9f40
MynaDima <dmytromyna@gmail.com>
2016-05-02 17:37:38 +0300

    student example

---
+++ src/main/webapp/frontend/module/student/appform/appFormService.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dima on 02.05.16.
+ */
+'use strict';
+
+function appFormService(http) {
+    var service = {};
+    
+    return service;
+}
+
+angular.module('appForm')
+    .service('appFormService', ['$http', appFormService]);
