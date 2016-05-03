b5e70cf4412f1c8d1170d60a79d1c07b3ef1b565
chalienko13 <dmitriy.chalienko@gmail.com>
2016-05-03 20:41:44 +0300

    Added FormQuestionAdapter to GsonFactory, refactoring(GsonFactory with lazy init)

--- src/main/java/ua/kpi/nc/persistence/model/adapter/GsonFactory.java
+++ src/main/java/ua/kpi/nc/persistence/model/adapter/GsonFactory.java
@@ -4,21 +4,42 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import ua.kpi.nc.persistence.model.User;
 import ua.kpi.nc.persistence.model.impl.real.ApplicationFormImpl;
+import ua.kpi.nc.persistence.model.impl.real.FormQuestionImpl;
 import ua.kpi.nc.persistence.model.impl.real.UserImpl;
 
 /**
  * Created by Chalienko on 25.04.2016.
  */
 public class GsonFactory {
+    private static Gson applicationFormGson;
+    private static Gson userGson;
+    private static Gson formQuestionGson;
+
     public static Gson getApplicationFormGson() {
-        return new GsonBuilder().registerTypeAdapter(ApplicationFormImpl.class, new ApplicationFormAdapter())
-                .setPrettyPrinting()
-                .create();
+        if (applicationFormGson == null){
+            applicationFormGson = new GsonBuilder().registerTypeAdapter(ApplicationFormImpl.class, new ApplicationFormAdapter())
+                    .setPrettyPrinting()
+                    .create();
+        }
+        return applicationFormGson;
     }
 
-    public static Gson getUserAdapter(){
-        return new GsonBuilder().registerTypeAdapter(UserImpl.class, new UserAdapter())
-                .setPrettyPrinting()
-                .create();
+    public static Gson getUserGson(){
+        if (userGson == null) {
+            userGson = new GsonBuilder().registerTypeAdapter(UserImpl.class, new UserAdapter())
+                    .setPrettyPrinting()
+                    .create();
+        }
+        return userGson;
+    }
+
+    public static Gson getFormQuestionGson() {
+        if (formQuestionGson == null) {
+            formQuestionGson = new GsonBuilder().registerTypeAdapter(FormQuestionImpl.class, new FormQuestionAdapter())
+                    .setPrettyPrinting()
+                    .create();
+
+        }
+        return formQuestionGson;
     }
 }
