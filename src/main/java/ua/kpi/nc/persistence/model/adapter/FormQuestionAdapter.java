7043d4bcb5b13d8ea23b0e1fc47f15a31e5bb320
chalienko13 <dmitriy.chalienko@gmail.com>
2016-05-03 20:22:58 +0300

    Added FormQuestionAdapter

---
+++ src/main/java/ua/kpi/nc/persistence/model/adapter/FormQuestionAdapter.java
@@ -0,0 +1,24 @@
+package ua.kpi.nc.persistence.model.adapter;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import ua.kpi.nc.persistence.model.ApplicationForm;
+import ua.kpi.nc.persistence.model.FormQuestion;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by Chalienko on 03.05.2016.
+ */
+public class FormQuestionAdapter implements JsonSerializer<FormQuestion> {
+    @Override
+    public JsonElement serialize(FormQuestion formQuestion, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("title", formQuestion.getTitle());
+        jsonObject.addProperty("type", formQuestion.getQuestionType().getTypeTitle());
+        return jsonObject;
+    }
+
+}
