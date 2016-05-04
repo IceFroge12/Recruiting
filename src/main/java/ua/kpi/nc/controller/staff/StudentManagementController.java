5ccb150343e11dfc3e32f3b82c5022238c234114
Sajel <sajel15@gmail.com>
2016-05-04 17:27:07 +0300

    fix

--- src/main/java/ua/kpi/nc/controller/staff/StudentManagementController.java
+++ src/main/java/ua/kpi/nc/controller/staff/StudentManagementController.java
@@ -58,47 +58,47 @@ public class StudentManagementController {
 		ApplicationForm applicationForm = applicationFormService.getApplicationFormById(id);
 		return gson.toJson(applicationFormToJson(applicationForm));
 	}
-	//TODO deasign and assign mapped - "assign/{id}", method - Post
-//	@RequestMapping(value = "assign/{id}", method = RequestMethod.GET)
-//	@ResponseBody
-//	public String assignStudent(@PathVariable Long id) {
-//		User interviewer = ServiceFactory.getUserService().getUserByID(33L);//GET CURRENT USER
-//		ApplicationFormService applicationFormService = ServiceFactory.getApplicationFormService();
-//		ApplicationForm applicationForm = applicationFormService.getApplicationFormById(id);
-//		if (!isApplicaionFormActual(applicationForm)) {
-//			return null;
-//		}
-//		InterviewService interviewService = ServiceFactory.getInterviewService();
-//		List<Interview> interviews = interviewService.getByApplicationForm(applicationForm);
-//		for (Interview interview : interviews) {
-//			for (Role interviewRole : interviewer.getRoles()) {
-//				if (Objects.equals(interview.getRole(), interviewRole)) {
-//					return "This student was already assigned to this type of interviewer";
-//				}
-//			}
-//		}
-//		Interview interview = new InterviewImpl();
-//		interview.setInterviewer(interviewer);
-//		interview.setApplicationForm(applicationForm);
-//		for (Role role : interviewer.getRoles()) {
-//			if (role.getRoleName().equals(RoleEnum.SOFT.name()) || role.getRoleName().equals(RoleEnum.TECH.name())) {
-//				interview.setRole(role);
-//			}
-//		}
-//		FormQuestionService questionService = ServiceFactory.getFormQuestionService();
-//		List<FormQuestion> questions = questionService.getByRole(interview.getRole());
-//		List<FormAnswer> answers = new ArrayList<>();
-//		for (FormQuestion formQuestion : questions) {
-//			if (formQuestion.isEnable()) {
-//				FormAnswer formAnswer = new FormAnswerImpl();
-//				formAnswer.setFormQuestion(formQuestion);
-//				formAnswer.setInterview(interview);
-//				answers.add(formAnswer);
-//			}
-//		}
-//		interviewService.insertInterviewWithAnswers(interview, answers);
-//		return "Student assigned";
-//	}
+	
+	@RequestMapping(value = "assign/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String assignStudent(@PathVariable Long id) {
+		User interviewer = ServiceFactory.getUserService().getUserByID(33L);//GET CURRENT USER
+		ApplicationFormService applicationFormService = ServiceFactory.getApplicationFormService();
+		ApplicationForm applicationForm = applicationFormService.getApplicationFormById(id);
+		if (!isApplicaionFormActual(applicationForm)) {
+			return null;
+		}
+		InterviewService interviewService = ServiceFactory.getInterviewService();
+		List<Interview> interviews = interviewService.getByApplicationForm(applicationForm);
+		for (Interview interview : interviews) {
+			for (Role interviewRole : interviewer.getRoles()) {
+				if (Objects.equals(interview.getRole(), interviewRole)) {
+					return "This student was already assigned to this type of interviewer";
+				}
+			}
+		}
+		Interview interview = new InterviewImpl();
+		interview.setInterviewer(interviewer);
+		interview.setApplicationForm(applicationForm);
+		for (Role role : interviewer.getRoles()) {
+			if (role.getRoleName().equals(RoleEnum.SOFT.name()) || role.getRoleName().equals(RoleEnum.TECH.name())) {
+				interview.setRole(role);
+			}
+		}
+		FormQuestionService questionService = ServiceFactory.getFormQuestionService();
+		List<FormQuestion> questions = questionService.getByRole(interview.getRole());
+		List<FormAnswer> answers = new ArrayList<>();
+		for (FormQuestion formQuestion : questions) {
+			if (formQuestion.isEnable()) {
+				FormAnswer formAnswer = new FormAnswerImpl();
+				formAnswer.setFormQuestion(formQuestion);
+				formAnswer.setInterview(interview);
+				answers.add(formAnswer);
+			}
+		}
+		interviewService.insertInterviewWithAnswers(interview, answers);
+		return "Student assigned";
+	}
 
 	@RequestMapping(value = "deassign/{id}", method = RequestMethod.GET)
 	@ResponseBody
