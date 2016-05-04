c67f4debff0640fbb0564f7f6a0c6326b3704f6a
Alona Vit <Alona Vit>
2016-05-04 20:36:00 +0300

    appForm checkboxes fix

--- src/main/webapp/frontend/module/home/home.js
+++ src/main/webapp/frontend/module/home/home.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('appHome', ['appHeader','appFooter','appMenuMain',"appSecondHeader",'appAuthorization']);
+angular.module('appHome', ['appHeader', 'appFooter', 'appMenuMain', "appSecondHeader", 'appAuthorization']);
 
 
 app.directive('appHeader', function () {
@@ -16,18 +16,44 @@ app.directive('appSecondHeader', function () {
     };
 });
 
-app.directive('appMenuMain',function () {
+app.directive('appMenuMain', function () {
     return {
         templateUrl: 'module/home/menu/mainMenu.html',
         controller: 'menuController'
     };
 });
 
-app.directive('appFooter',function () {
+app.directive('appFooter', function () {
     return {
         templateUrl: 'module/footer/footer.html',
         controller: 'footerController'
     };
 });
+
+app.directive('navMenu', function ($location) {
+        return function (scope, element, attrs) {
+            var links = element.find('a'),
+                link,
+                currentLink,
+                urlMap = {},
+                i;
+            for (i = 0; i < links.length; i++) {
+                link = angular.element(links[i]);
+                urlMap[link.attr('href')] = link;
+            }
+
+            scope.$on('$routeChangeStart', function () {
+                var pathLink = urlMap[$location.path()];
+
+                if (pathLink) {
+                    if (currentLink) {
+                        curremtLink.removeClass('on');
+                    }
+                    currentLink = pathLink;
+                    currentLink.addClass('on');
+                }
+            });
+        };
+    });
 
 
