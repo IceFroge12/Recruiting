de0a62508fba572faa1208cd1e3931dc5f3b18cf
Alona Vit <Alona Vit>
2016-05-06 11:14:03 +0300

    fix

---
+++ src/main/webapp/frontend/module/student/settings/studentSettings.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Alona on 06.05.2016.
+ */
+
+'use strict';
+
+angular.module('appStudentSettings', []);
---
+++ src/main/webapp/frontend/module/student/settings/studentSettingsController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Alona on 06.05.2016.
+ */
+function studentSettingsController($scope) {
+
+}
+
+angular.module('appStudentSettings')
+    .controller('studentSettingsController', ['$scope', studentSettingsController]);
