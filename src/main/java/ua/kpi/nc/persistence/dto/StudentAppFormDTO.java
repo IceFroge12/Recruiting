cff18cc913afce2c556d9b0eb91a6ed0db57b650
MynaDima <3992155Op>
2016-05-13 14:22:48 +0300

    fix frontend studmanagement controller

---
+++ src/main/java/ua/kpi/nc/persistence/dto/StudentAppFormDTO.java
@@ -0,0 +1,91 @@
+package ua.kpi.nc.persistence.dto;
+
+import ua.kpi.nc.persistence.model.Status;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 13.05.2016.
+ */
+public class StudentAppFormDto {
+
+    private Long id;
+    private Long appFormId;
+    private String firstName;
+    private String lastName;
+    private String status;
+    private List<Status> possibleStatus;
+
+    public StudentAppFormDto() {
+    }
+
+    public StudentAppFormDto(Long id, Long appFormId, String firstName,
+                             String lastName, String status, List<Status> possibleStatus) {
+        this.id = id;
+        this.appFormId = appFormId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+        this.possibleStatus = possibleStatus;
+    }
+
+    public List<Status> getPossibleStatus() {
+        return possibleStatus;
+    }
+
+    public void setPossibleStatus(List<Status> possibleStatus) {
+        this.possibleStatus = possibleStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAppFormId() {
+        return appFormId;
+    }
+
+    public void setAppFormId(Long appFormId) {
+        this.appFormId = appFormId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentAppFormDto{" +
+                "id=" + id +
+                ", appFormId=" + appFormId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", status='" + status + '\'' +
+                ", possibleStatus=" + possibleStatus +
+                '}';
+    }
+}
