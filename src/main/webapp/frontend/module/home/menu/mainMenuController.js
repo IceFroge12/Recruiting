4df546d820307b1841544a3f3904ed9fae188ad5
MynaDima <3992155Op>
2016-05-16 14:41:56 +0300

    add  menu paragraph active

--- src/main/webapp/frontend/module/home/menu/mainMenuController.js
+++ src/main/webapp/frontend/module/home/menu/mainMenuController.js
@@ -5,6 +5,11 @@
 'use strict';
 
 function mainMenuController($scope, $location) {
+
+    $scope.getClass = function (path) {
+        return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+    };
+
     $scope.educationnc = function () {
         $location.path("/educationNC");
     };
