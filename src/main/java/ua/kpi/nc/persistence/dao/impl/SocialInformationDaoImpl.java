3f8c5ca352d78bfd6fb503eaf5c668e9a6e3028e
Korzh Vladimir <korzh.volod@gmail.com>
2016-05-29 14:18:07 +0300

    Social Information Update

--- src/main/java/ua/kpi/nc/persistence/dao/impl/SocialInformationDaoImpl.java
+++ src/main/java/ua/kpi/nc/persistence/dao/impl/SocialInformationDaoImpl.java
@@ -35,23 +35,32 @@ public class SocialInformationDaoImpl extends JdbcDaoSupport implements SocialIn
         SocialInformation socialInformation;
         socialInformation = new SocialInformationImpl(socialInformationId,
                 resultSet.getString("access_info"), new UserProxy(resultSet.getLong("id_user")),
-                new SocialNetwork(resultSet.getLong("id_social_network"), resultSet.getString("title")));
+                new SocialNetwork(resultSet.getLong("id_social_network"), resultSet.getString("title")),resultSet.getLong("id_user_in_social_network"));
         return socialInformation;
     };
 
-    private static final String SQL_GET_BY_ID = "SELECT si.id, si.access_info, si.id_user, si.id_social_network, sn.title FROM public.social_information si JOIN public.social_network sn ON si.id_social_network = sn.id WHERE si.id = ?;";
+    private static final String SQL_GET_BY_ID = "SELECT si.id, si.access_info, si.id_user, si.id_social_network, sn.title, si.id_user_in_social_network " +
+            "FROM public.social_information si JOIN public.social_network sn ON si.id_social_network = sn.id WHERE si.id = ?;";
 
-    private static final String SQL_GET_BY_USER_ID = "SELECT si.id, si.id_social_network, si.id_user, si.access_info, sn.title"
+    private static final String SQL_GET_BY_USER_ID = "SELECT si.id, si.id_social_network, si.id_user, si.access_info, sn.title, si.id_user_in_social_network "
             + "FROM \"social_information \" si" + "\n INNER JOIN social_network sn ON sn.id = si.id_social_network "
             + "WHERE si.id_user = ?";
 
-    private static final String SQL_INSERT = "INSERT INTO social_information (access_info, id_user, id_social_network) VALUES (?, ?, ?)";
+    private static final String SQL_INSERT = "INSERT INTO social_information (access_info, id_user, id_social_network, id_user_in_social_network) VALUES (?, ?, ?, ?)";
 
     private static final String SQL_UPDATE = "UPDATE social_information set access_info = ? WHERE social_information.id = ?";
 
     private static final String SQL_DELETE = "DELETE FROM \"social_information\" WHERE \"social_information\".id = ?";
 
-    private static final String SQL_GET_BY_USER_EMAIL_SOCIAL_TYPE = "SELECT si.id, si.id_social_network, si.id_user, si.access_info, sn.title FROM public.user u JOIN public.social_information si ON u.id = si.id_user JOIN public.social_network sn ON si.id_social_network = sn.id WHERE u.email = ? AND si.id_social_network = ?;";
+    private static final String SQL_GET_BY_USER_EMAIL_SOCIAL_TYPE = "SELECT si.id, si.id_social_network, si.id_user, si.access_info," +
+            " sn.title, si.id_user_in_social_network FROM public.user u JOIN public.social_information si ON u.id = si.id_user " +
+            "JOIN public.social_network sn ON si.id_social_network = sn.id WHERE u.email = ? AND si.id_social_network = ?;";
+
+    private static final String SQL_GET_BY_ID_IN_SOCIAL_NETWORK_AND_SOCIAL_TYPE = "SELECT si.id, si.access_info, si.id_user, si.id_social_network, sn.title, si.id_user_in_social_network " +
+            "FROM public.social_information si JOIN public.social_network sn ON si.id_social_network = sn.id WHERE si.id_social_network = ? and si.id_user_in_social_network = ?;";
+
+    private static final String SQL_EXIST_ID_IN_SOCIAL_NETWORK_AND_SOCIAL_TYPE = "SELECT EXISTS( SELECT si.id, si.access_info, si.id_user, si.id_social_network, sn.title, si.id_user_in_social_network \" +\n" +
+            "            \"FROM public.social_information si JOIN public.social_network sn ON si.id_social_network = sn.id WHERE si.id_social_network = ? and si.id_user_in_social_network = ?);";
 
     @Override
     public SocialInformation getById(Long id) {
@@ -97,6 +106,18 @@ public class SocialInformationDaoImpl extends JdbcDaoSupport implements SocialIn
     public boolean isExist(String email, Long idSocialNetwork) {
         log.trace("Search user social information by email = {}", email);
         return getByUserEmailSocialType(email, idSocialNetwork) != null;
+    }
+
+    @Override
+    public SocialInformation getByIdUserInSocialNetworkSocialType(Long idUserInSocialNetwork, Long idSocialNetwork) {
+        log.trace("Search user social information by Id User In Social Network and Social Type = {}, {}",idUserInSocialNetwork,idSocialNetwork);
+        return this.getJdbcTemplate().queryWithParameters(SQL_GET_BY_ID_IN_SOCIAL_NETWORK_AND_SOCIAL_TYPE, extractor,idSocialNetwork,idUserInSocialNetwork);
+    }
+
+    @Override
+    public boolean isExist(Long idUserInSocialNetwork, Long idSocialNetwork) {
+        log.trace("Search user exists social information by Id User In Social Network and Social Type = {}, {}",idUserInSocialNetwork,idSocialNetwork);
+        return this.getJdbcTemplate().queryWithParameters(SQL_EXIST_ID_IN_SOCIAL_NETWORK_AND_SOCIAL_TYPE, resultSet -> resultSet.getBoolean(1),idSocialNetwork,idUserInSocialNetwork );
     }
 
     @Override
